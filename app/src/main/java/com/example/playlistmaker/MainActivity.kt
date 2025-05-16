6b706dc53f3a032885bdd4923137a647e870c9bf
lizaKotsar <yelizaveta.kotsar@bk.ru>
2025-05-16 15:14:16 +0300

    sprint 9 gooooooodworkshare

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -11,6 +11,8 @@ import android.widget.Button
 import android.content.Intent
 import android.util.Log
 import android.util.TypedValue
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,13 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.ImageButton
+import android.widget.LinearLayout
 
 
 class SettingsActivity : AppCompatActivity() {
@@ -18,7 +20,18 @@ class SettingsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // кнопки на экране настроек
 
+        // Кнопка поделиться
+        val shareButton = findViewById<LinearLayout>(R.id.share_button)
+
+        shareButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text))
+            }
+            startActivity(Intent.createChooser(shareIntent, null))
+        }
         val backButton = findViewById<ImageButton>(R.id.back_button)
         backButton.setOnClickListener {
             finish()
