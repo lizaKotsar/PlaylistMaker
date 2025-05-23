c5aa7736ffc452396a2c4c070282b7bf9675ecf5
lizaKotsar <yelizaveta.kotsar@bk.ru>
2025-05-23 03:15:35 +0300

    sprint10work2

--- app/src/main/java/com/example/playlistmaker/data/model/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/data/model/TrackViewHolder.kt
@@ -20,6 +20,7 @@ class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         Glide.with(itemView)
             .load(track.artworkUrl100)
             .placeholder(R.drawable.image_placeholder)
+            .error(R.drawable.image_placeholder)
             .centerCrop()
             .into(trackArtwork)
     }
